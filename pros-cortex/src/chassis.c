f85cf7f71058826c5f315d59f922ffaf76bfb870
Jason <27450071+jyao6429@users.noreply.github.com>
2019-06-17 17:59:54 -0400

    Delete broken project

--- pros-cortex/src/chassis.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-void powerMotors(int leftPower, int rightPower)
-{
-  motorSet(PORT_leftBackMotor, leftPower);
-  motorSet(PORT_leftFrontMotor, leftPower);
-  motorSet(PORT_rightBackMotor, rightPower);
-  motorSet(PORT_rightFrontMotor, rightPower);
-}
