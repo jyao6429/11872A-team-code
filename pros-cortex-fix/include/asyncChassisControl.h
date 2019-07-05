711266b73f12993dd3e1ab058f8b6aa48639eb2f
jyao6429 <27450071+jyao6429@users.noreply.github.com>
2019-07-05 14:53:06 -0400

    Changed asyncChassisControl so task only runs when needed, added documentation

--- pros-cortex-fix/include/asyncChassisControl.h
+++ pros-cortex-fix/include/asyncChassisControl.h
@@ -15,6 +15,7 @@ typedef enum AsyncChassisOptions
 } AsyncChassisOptions;
 
 // Structs as containers for motion parameters
+// Container for moveToTarget motions
 typedef struct MTTContainer
 {
   double angle, distance, targetX, targetY, startX, startY, maxErrorX, decelEarly, dropEarly;
@@ -23,7 +24,7 @@ typedef struct MTTContainer
   MTTMode mode;
   bool isDegrees;
 } MTTContainer;
-
+// Container for sweepTurnToTarget motion
 typedef struct TTTSweepContainer
 {
   double targetX, targetY, targetAngle, targetRadius;
@@ -31,7 +32,7 @@ typedef struct TTTSweepContainer
   int power;
   bool isAccurate, isDegrees;
 } TTTSweepContainer;
-
+// Container for turnToAngleNew and turnToTargetNew motions
 typedef struct TTTRegularContainer
 {
   double targetX, targetY, targetAngle, fullPowerRatio, stopPowerDiff, angleOffset;
@@ -41,20 +42,103 @@ typedef struct TTTRegularContainer
 } TTTRegularContainer;
 
 // Variables for handling async tasks
-bool isChassisMoving;
+// Keeps track when the robot is performing a move
+bool isChassisMoving = false;
+// Handles the async task
 TaskHandle asyncChassisHandle;
-AsyncChassisOptions nextMove;
+// The next move to be performed by the robot
+AsyncChassisOptions nextMove = ASYNC_NONE;
+// Container variables for each motion
 MTTContainer mttContainer;
 TTTSweepContainer sweepContainer;
 TTTRegularContainer turnContainer;
 
+/**
+ * Waits until the robot completes the current motion
+ */
+void waitUntilChassisMoveComplete();
+/**
+ * Initializes the async controller task
+ */
 void initializeAsyncChassisController();
+/**
+ * Stops the async controller and resets variables to defaults
+ */
 void stopAsyncChassisController();
 
+/**
+ * Moves to a desired position along a line connecting the target and starting points
+ *
+ * @param targetX - the x component of the target point
+ * @param targetY - the y component of the target point
+ * @param startX - the x component of the starting point
+ * @param startY - the y component of the starting point
+ * @param power - (-127 - 127) the power to travel the line, negatives means the robot travels backwards
+ * @param startPower - the starting power to ramp up to power (No use with MTT_SIMPLE)
+ * @param maxErrorX - (e.g. 0.5 - 1) the maxiumum error perpendicular to the line in inches
+ * @param decelEarly - (0 - 20) the distance from the target when to use the constant decelPower
+ * @param decelPower - (0 - 50) the power to use when decelerating
+ * @param dropEarly - (0 - 15) the distance from the target when to start breaking if needed, this also affects the deceleration point
+ * @param stopType - the StopType to use to park
+ * @param mode - the MTTMode to use to travel the line
+ */
 void moveToTargetSimpleAsync(double targetX, double targetY, double startX, double startY, int power, int startPower, double maxErrorX, double decelEarly, int decelPower, double dropEarly, StopType stopType, MTTMode mode);
+/**
+ * Moves a desired distance along a line in the direction of the given angle
+ *
+ * @param angle - the orientation to move in
+ * @param distance - the distance to travel in inches
+ * @param startX - the x component of the starting point
+ * @param startY - the y component of the starting point
+ * @param power - (-127 - 127) the power to travel the line, negatives means the robot travels backwards
+ * @param startPower - the starting power to ramp up to power (No use with MTT_SIMPLE)
+ * @param maxErrorX - (e.g. 0.5 - 1) the maxiumum error perpendicular to the line in inches
+ * @param decelEarly - (0 - 20) the distance from the target when to use the constant decelPower
+ * @param decelPower - (0 - 50) the power to use when decelerating
+ * @param dropEarly - (0 - 15) the distance from the target when to start breaking if needed, this also affects the deceleration point
+ * @param stopType - the StopType to use to park
+ * @param mode - the MTTMode to use to travel the line
+ * @param isDegrees - if the angle is given in degrees
+ */
 void moveToTargetDisSimpleAsync(double angle, double distance, double startX, double startY, int power, int startPower, double maxErrorX, double decelEarly, int decelPower, double dropEarly, StopType stopType, MTTMode mode, bool isDegrees);
+/**
+ * Turns to face a specific point with an offset if desired
+ *
+ * @param targetX - the x component of the target point
+ * @param targetY - the y component of the target point
+ * @param targetAngle - the angle to sweep to
+ * @param targetRadius - (e.g. 10 - 20) the radius of the circle to follow in inches
+ * @param turnDir - the direction to turn based on TurnDir enum (TURN_CH = chooses automatically)
+ * @param power - (-127 - 127) the power to take the turn, negatives means the robot travels backwards
+ * @param isAccurate - if the sweeps should be more accurate
+ * @param isDegrees - if the targetAngle is given in degrees
+ */
 void sweepTurnToTargetAsync(double targetX, double targetY, double targetAngle, double targetRadius, TurnDir turnDir, int power, bool isAccurate, bool isDegrees);
+/**
+ * Turns to a specified orientation
+ *
+ * @param targetAngle - the angle to turn to
+ * @param turnDir - the direction to turn based on TurnDir enum (TURN_CH = chooses automatically)
+ * @param fullPowerRatio - (0.0 - 1.0) ratio of the turn taken in full power, depends on speed and accuracy needed
+ * @param coastPower - (e.g. 20 - 30) the power used to turn after full power, also depends on speed and accuracy needed
+ * @param stopPowerDiff - (e.g. 10 - 15) the angle error in degrees to cut the motors
+ * @param harshStop - if the robot should brake at the end of the turn
+ * @param isDegrees - if the targetAngle is given in degrees
+ */
 void turnToAngleNewAsync(double targetAngle, TurnDir turnDir, double fullPowerRatio, int coastPower, double stopPowerDiff, bool harshStop, bool isDegrees);
+/**
+ * Turns to face a specific point with an offset if desired
+ *
+ * @param targetX - the x component of the target point
+ * @param targetY - the y component of the target point
+ * @param turnDir - the direction to turn based on TurnDir enum (TURN_CH = chooses automatically)
+ * @param fullPowerRatio - (0.0 - 1.0) ratio of the turn taken in full power, depends on speed and accuracy needed
+ * @param coastPower - (e.g. 20 - 30) the power used to turn after full power, also depends on speed and accuracy needed
+ * @param stopPowerDiff - (e.g. 10 - 15) the angle error in degrees to cut the motors
+ * @param angleOffset - the desired offset to the angle directly facing the point
+ * @param harshStop - if the robot should brake at the end of the turn
+ * @param isDegrees - if the angleOffset is given in degrees
+ */
 void turnToTargetNewAsync(double targetX, double targetY, TurnDir turnDir, double fullPowerRatio, int coastPower, double stopPowerDiff, double angleOffset, bool harshStop, bool isDegrees);
 
 #endif
--- pros-cortex-fix/src/asyncChassisControl.c
+++ pros-cortex-fix/src/asyncChassisControl.c
@@ -2,38 +2,41 @@
 
 void asyncChassisTask(void *ignore)
 {
-  while (true)
+  if (nextMove != ASYNC_NONE)
   {
-    if (nextMove != ASYNC_NONE)
+    isChassisMoving = true;
+    switch (nextMove)
     {
-      isChassisMoving = true;
-      switch (nextMove)
-      {
-        case ASYNC_MTT_SIMPLE:
-          moveToTargetSimple(mttContainer.targetX, mttContainer.targetY, mttContainer.startX, mttContainer.startY, mttContainer.power, mttContainer.startPower, mttContainer.maxErrorX, mttContainer.decelEarly, mttContainer.decelPower, mttContainer.dropEarly, mttContainer.stopType, mttContainer.mode);
-          break;
-        case ASYNC_MTT_DIS:
-          moveToTargetDisSimple(mttContainer.angle, mttContainer.distance, mttContainer.startX, mttContainer.startY, mttContainer.power, mttContainer.startPower, mttContainer.maxErrorX, mttContainer.decelEarly, mttContainer.decelPower, mttContainer.dropEarly, mttContainer.stopType, mttContainer.mode, mttContainer.isDegrees);
-          break;
-        case ASYNC_TTT_SWEEP:
-          sweepTurnToTarget(sweepContainer.targetX, sweepContainer.targetY, sweepContainer.targetAngle, sweepContainer.targetRadius, sweepContainer.turnDir, sweepContainer.power, sweepContainer.isAccurate, sweepContainer.isDegrees);
-          break;
-        case ASYNC_TTT_ANGLE:
-          turnToAngleNew(turnContainer.targetAngle, turnContainer.turnDir, turnContainer.fullPowerRatio, turnContainer.coastPower, turnContainer.stopPowerDiff, turnContainer.harshStop, turnContainer.isDegrees);
-          break;
-        case ASYNC_TTT_TARGET:
-          turnToTargetNew(turnContainer.targetX, turnContainer.targetY, turnContainer.turnDir, turnContainer.fullPowerRatio, turnContainer.coastPower, turnContainer.stopPowerDiff, turnContainer.angleOffset, turnContainer.harshStop, turnContainer.isDegrees);
-          break;
-      }
-      isChassisMoving = false;
-      nextMove = ASYNC_NONE;
+      case ASYNC_MTT_SIMPLE:
+        moveToTargetSimple(mttContainer.targetX, mttContainer.targetY, mttContainer.startX, mttContainer.startY, mttContainer.power, mttContainer.startPower, mttContainer.maxErrorX, mttContainer.decelEarly, mttContainer.decelPower, mttContainer.dropEarly, mttContainer.stopType, mttContainer.mode);
+        break;
+      case ASYNC_MTT_DIS:
+        moveToTargetDisSimple(mttContainer.angle, mttContainer.distance, mttContainer.startX, mttContainer.startY, mttContainer.power, mttContainer.startPower, mttContainer.maxErrorX, mttContainer.decelEarly, mttContainer.decelPower, mttContainer.dropEarly, mttContainer.stopType, mttContainer.mode, mttContainer.isDegrees);
+        break;
+      case ASYNC_TTT_SWEEP:
+        sweepTurnToTarget(sweepContainer.targetX, sweepContainer.targetY, sweepContainer.targetAngle, sweepContainer.targetRadius, sweepContainer.turnDir, sweepContainer.power, sweepContainer.isAccurate, sweepContainer.isDegrees);
+        break;
+      case ASYNC_TTT_ANGLE:
+        turnToAngleNew(turnContainer.targetAngle, turnContainer.turnDir, turnContainer.fullPowerRatio, turnContainer.coastPower, turnContainer.stopPowerDiff, turnContainer.harshStop, turnContainer.isDegrees);
+        break;
+      case ASYNC_TTT_TARGET:
+        turnToTargetNew(turnContainer.targetX, turnContainer.targetY, turnContainer.turnDir, turnContainer.fullPowerRatio, turnContainer.coastPower, turnContainer.stopPowerDiff, turnContainer.angleOffset, turnContainer.harshStop, turnContainer.isDegrees);
+        break;
     }
-    delay(50);
+    isChassisMoving = false;
+    nextMove = ASYNC_NONE;
   }
+}
+void waitUntilChassisMoveComplete()
+{
+  while (isChassisMoving) { delay(40); }
 }
 void initializeAsyncChassisController()
 {
-  stopAsyncChassisController();
+  // Stop task if needed
+  unsigned int asyncState = taskGetState(asyncChassisHandle);
+  if (asyncChassisHandle != NULL && (asyncState == TASK_RUNNING || asyncState == TASK_SLEEPING || asyncState == TASK_SUSPENDED))
+    taskDelete(APSTask);
 
   asyncChassisHandle = taskCreate(asyncChassisTask, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT + 1);
 }
@@ -62,8 +65,9 @@ void moveToTargetSimpleAsync(double targetX, double targetY, double startX, doub
   mttContainer.dropEarly = dropEarly;
   mttContainer.stopType = stopType;
   mttContainer.mode = mode;
-  // Set nextMove
+  // Set nextMove and run the controller
   nextMove = ASYNC_MTT_SIMPLE;
+  initializeAsyncChassisController();
 }
 void moveToTargetDisSimpleAsync(double angle, double distance, double startX, double startY, int power, int startPower, double maxErrorX, double decelEarly, int decelPower, double dropEarly, StopType stopType, MTTMode mode, bool isDegrees)
 {
@@ -81,8 +85,9 @@ void moveToTargetDisSimpleAsync(double angle, double distance, double startX, do
   mttContainer.stopType = stopType;
   mttContainer.mode = mode;
   mttContainer.isDegrees = isDegrees;
-  // Set nextMove
+  // Set nextMove and run the controller
   nextMove = ASYNC_MTT_DIS;
+  initializeAsyncChassisController();
 }
 void sweepTurnToTargetAsync(double targetX, double targetY, double targetAngle, double targetRadius, TurnDir turnDir, int power, bool isAccurate, bool isDegrees)
 {
@@ -95,8 +100,9 @@ void sweepTurnToTargetAsync(double targetX, double targetY, double targetAngle, 
   sweepContainer.power = power;
   sweepContainer.isAccurate = isAccurate;
   sweepContainer.isDegrees = isDegrees;
-  // Set nextMove
+  // Set nextMove and run the controller
   nextMove = ASYNC_TTT_SWEEP;
+  initializeAsyncChassisController();
 }
 void turnToAngleNewAsync(double targetAngle, TurnDir turnDir, double fullPowerRatio, int coastPower, double stopPowerDiff, bool harshStop, bool isDegrees)
 {
@@ -108,8 +114,9 @@ void turnToAngleNewAsync(double targetAngle, TurnDir turnDir, double fullPowerRa
   turnContainer.stopPowerDiff = stopPowerDiff;
   turnContainer.harshStop = harshStop;
   turnContainer.isDegrees = isDegrees;
-  // Set nextMove
+  // Set nextMove and run the controller
   nextMove = ASYNC_TTT_ANGLE;
+  initializeAsyncChassisController();
 }
 void turnToTargetNewAsync(double targetX, double targetY, TurnDir turnDir, double fullPowerRatio, int coastPower, double stopPowerDiff, double angleOffset, bool harshStop, bool isDegrees)
 {
@@ -123,6 +130,7 @@ void turnToTargetNewAsync(double targetX, double targetY, TurnDir turnDir, doubl
   turnContainer.angleOffset = angleOffset;
   turnContainer.harshStop = harshStop;
   turnContainer.isDegrees = isDegrees;
-  // Set nextMove
+  // Set nextMove and run the controller
   nextMove = ASYNC_TTT_TARGET;
+  initializeAsyncChassisController();
 }
