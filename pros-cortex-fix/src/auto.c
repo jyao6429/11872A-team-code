a1937ad83aad957d6df9325c8dfd37e151cf7d4a
Jason <27450071+jyao6429@users.noreply.github.com>
2019-10-25 15:37:04 -0400

    Initial edits of autoRedSmallSafe

--- pros-cortex-fix/src/auto.c
+++ pros-cortex-fix/src/auto.c
@@ -72,10 +72,11 @@ void autoBlueSmallSafe()
   waitUntilChassisMoveComplete();
   score();
 
-  // 5. Back away from the stack and tilt the tray back
+  // 5. Back away from the stack and tilt the tray back and return the arms down
   moveToTargetDisSimpleAsync(45.0, 6.0, 12.0, 12.0, -30, 0, 1.0, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL, true);
   delay(1000);
   moveTrayAngled();
+  moveArmsZero();
   waitUntilChassisMoveComplete();
 }
 void autoBlueLarge()
@@ -94,27 +95,28 @@ void autoRedSmallSafe()
 
   // 1. Start rollers and drive forward to collect preload and 4 cubes
   setRollers(127);
-  moveToTargetSimpleAsync(26.4, 50.0, 26.4, BACK_TO_CENTER, 127, 0, 1.0, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL);
+  moveToTargetSimpleAsync(FIELD_WIDTH - 26.4, 50.0, FIELD_WIDTH - 26.4, BACK_TO_CENTER, 127, 0, 1.0, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL);
   waitUntilChassisMoveComplete();
 
   // 2. Stop rollers and drive backwards to diagonal for scoring
   setRollers(0);
-  moveToTargetSimpleAsync(26.4, 26.4, 26.4, globalPose.y, -127, 0, 0.5, 0, 20, 0, STOP_SOFT, MTT_CASCADING);
+  moveToTargetSimpleAsync(FIELD_WIDTH - 26.4, 26.4, FIELD_WIDTH - 26.4, globalPose.y, -127, 0, 0.5, 0, 20, 0, STOP_SOFT, MTT_CASCADING);
   waitUntilChassisMoveComplete();
 
   // 3. Turn towards small goal
-  turnToTargetNewAsync(0.0, 0.0, TURN_CCW, 0.6, 25, 10, 0.0, true, true);
+  turnToTargetNewAsync(FIELD_WIDTH, 0.0, TURN_CW, 0.6, 25, 10, 0.0, true, true);
   waitUntilChassisMoveComplete();
 
   // 4. Drive towards goal, and score the stack
-  moveToTargetSimpleAsync(15.0, 15.0, 26.4, 26.4, 100, 0, 0.5, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL);
+  moveToTargetSimpleAsync(FIELD_WIDTH - 15.0, 15.0, FIELD_WIDTH - 26.4, 26.4, 100, 0, 0.5, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL);
   waitUntilChassisMoveComplete();
   score();
 
-  // 5. Back away from the stack and tilt the tray back
-  moveToTargetDisSimpleAsync(45.0, 6.0, 12.0, 12.0, -30, 0, 1.0, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL, true);
+  // 5. Back away from the stack and tilt the tray back and return the arms down
+  moveToTargetDisSimpleAsync(-45.0, 6.0, FIELD_WIDTH - 12.0, 12.0, -30, 0, 1.0, 0, 0, 0, STOP_NONE, MTT_PROPORTIONAL, true);
   delay(1000);
   moveTrayAngled();
+  moveArmsZero();
   waitUntilChassisMoveComplete();
 }
 void autoRedLarge()
